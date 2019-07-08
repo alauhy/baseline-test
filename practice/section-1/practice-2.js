b35027e1a10d8672f22e0e40021bfd5848cd9547
alauhy <39290088+alauhy@users.noreply.github.com>
2019-07-08 12:33:11 +0800

    Update practice-2.js

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,13 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+ var collectionC=[];
+  for(var i in collectionA){
+    for(var j in collectionB[0]){
+      if(collectionA[i]==collectionB[0][j]){
+        collectionC.push(collectionA[i]);
+      }
+    }
+  }
+  return collectionC;
 }
